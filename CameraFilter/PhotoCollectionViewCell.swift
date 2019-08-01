ce0a69d003444e8d2c016d194ee677752dbd6e01
APP技術部-陳冠志 <bruce@mitake.com.tw>
2019-08-01 18:12:28 +0800

    commit

---
+++ CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by APP技術部-陳冠志 on 2019/8/1.
+//  Copyright © 2019 Bruce Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var applyFilterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let navC = segue.destination as? UINavigationController, let photosCVC = navC.viewControllers.first as? PhotoCollectionViewController else {
+            fatalError("Segue destination is not found")
+        }
+        
+        photosCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            
+            DispatchQueue.main.async {
+                self?.updateUI(with: photo)
+            }
+            
+        }).disposed(by: disposeBag)
+    }
 
+    @IBAction func applyFilterButtonPressed() {
+        
+        guard let sourceImage = self.photoImageView.image else { return }
+        
+        FiltersService().applyFilter(to: sourceImage).subscribe(onNext: { filteredImage in
+            DispatchQueue.main.async {
+                self.photoImageView.image = filteredImage
+            }
+        }).disposed(by: disposeBag)
+        
+    }
+    
+    private func updateUI(with image: UIImage) {
+        self.photoImageView.image = image
+        self.applyFilterButton.isHidden = false
+    }
+    
+    
+    
 }
 
